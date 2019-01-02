903074279ce31062013879e6b32bedc842281939
13029768866 <13029768866@163.com>
2019-01-02 22:45:24 +0800

    parcel打包

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="wzj-button">按钮</button>
+</template>
+<script>
+   export default {
+
+   }
+</script>
+<style lang="scss">
+    .wzj-button{
+        font-size: var(--font-siez);
+        height:var(--button-height);
+        padding: 0 1em;
+        border-radius:var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg) ;
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
